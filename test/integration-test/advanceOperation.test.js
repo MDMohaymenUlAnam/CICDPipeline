be957dd3962d4c2250fae80bb338452f6af73534
MDMohaymenUlAnam <mohaymenulanamnihan@gmail.com>
2024-05-09 11:21:05 +0600

    corrected the value

--- test/integration-test/advanceOperation.test.js
+++ test/integration-test/advanceOperation.test.js
@@ -2,7 +2,7 @@ const calculator = require("../../src/advanceOperation");
 
 describe('Integration Testing', () => {
     describe.each([
-        [2, 3, 6, 2],
+        [2, 3, 8, 2],
         [5, 4, 625, 1], 
         [3, 12, 531441, 3], 
         [4, 6, 4096, 4] 
